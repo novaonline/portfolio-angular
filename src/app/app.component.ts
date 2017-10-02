1d08703f20d05fb27d58debfab4e72a4e6502091
Emmanuel Quagraine <eman_the_man@hotmail.com>
2017-10-02 17:41:31 -0500

    first attempt at adding a circle logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = `Hello, I'm Emmanuel`;
+  title = `Hello, I'm Emmanuel Quagraine`;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { JourneyComponent } from './components/journey/journey.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JourneyComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
